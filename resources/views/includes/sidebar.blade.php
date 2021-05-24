82a5ae1cbcdacbae5ce55f27554f516a0dd04e34
iqbalhanafi20 <iqbalhanafi436@gmail.com>
2021-05-24 10:48:57 +0700

    project travelology laravel first commit

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,28 @@
+<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                           
+                            <a class="nav-link">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Beranda
+                            </a>
+
+                            <a class="nav-link">
+                                <div class="sb-nav-link-icon"><i class="fas fa-hotel"></i></div>
+                                Paket Travel
+                            </a>
+
+                            <a class="nav-link">
+                                <div class="sb-nav-link-icon"><i class="fas fa-images"></i></div>
+                                Galeri Travel
+                            </a>
+
+                            <a class="nav-link">
+                                <div class="sb-nav-link-icon"><i class="fas fa-dollar-sign"></i></div>
+                                Transaksi
+                            </a>
+                          
+                        </div>
+                    </div>
+                  
+                </nav>
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - Travelology</title>
+        @include('includes.style')
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+
+    </head>
+    <body class="sb-nav-fixed">
+        
+        @include('includes/navbar')
+
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                @include('includes.sidebar')
+            </div>
+            <div id="layoutSidenav_content">
+                
+                @yield('content')
+
+
+                @include('includes/footer')
+
+            </div>
+        </div>
+        
+        @include('includes.script')
+
+    </body>
+</html>
